4c0cbf3e71d52393b466ab9352f3f9d6c89673ca
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-01-21 11:02:00 +0530

    Lab-2

--- Assignment-1/Question7.m
+++ Assignment-1/Question7.m
@@ -1,9 +1,16 @@
-function u = Question7(n)
-    if (n == 1 || n==0)
-        u = n; 
-        return;
-    elseif n>=2
-        u = Question7(n-2)+Question7(n-1);
-        fprintf('%i, ', u)
-    end
+fibonacci = [0 1];
+n=10;
+for i = 1:n-2
+    fibonacci = [fibonacci fibonacci(end)+fibonacci(end-1)];
 end
+disp(fibonacci)
+
+% function u = Question7(n)
+%     if (n == 1 || n==0)
+%         u = n; 
+%     elseif n>=2
+%         u = Question7(n-2)+Question7(n-1);
+%         fprintf('%i, ', u)
+%         return;
+%     end
+% end
