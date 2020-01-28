ed6ec94535c97d6300f21db1056df62aceb710c7
viveknigam3003 <viveknigam.nigam3@gmail.com>
2020-01-28 09:39:34 +0530

    Lab-2

---
+++ Assignment-2/Question1.m
@@ -0,0 +1,12 @@
+%%
+clc
+d = [1 2 3 4];
+D = diag(d);
+disp (D);
+
+%%
+clc
+A = [1:4;5:8;9:12];
+x = diag(A);
+disp (A);
+disp (x);
---
+++ Assignment-2/Question2.m
@@ -0,0 +1,31 @@
+clear all;
+
+%%
+clc;
+L1 = linspace(1, 20);
+disp(L1);
+
+%%
+clc;
+L1 = linspace(1, 20, 5);
+disp(L1);
+
+%%
+clc;
+L1 = logspace(2, 4);
+disp(L1);
+
+%%
+clc;
+L1 = logspace(1, 4, 8);
+disp(L1);
+
+%%
+clc;
+L1 = 2:10;
+disp(L1);
+
+%%
+clc;
+L1 = 2:1.5:20;
+disp(L1);
